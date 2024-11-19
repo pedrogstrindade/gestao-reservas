6cb53220032f219a4560d7ad768539d769268ce2
pedrogstrindade <ppeedrotrindade82@gmail.com>
2024-11-18 21:30:24 -0300

    Initial commit

---
+++ src/main/java/com/gestao/api/GestaoReservasApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoReservasApplication.class, args);
+	}
+
+}
